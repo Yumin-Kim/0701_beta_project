4def8044e41e17f01a573eb0c9039a1ef78277cf
Yumin-Kim <dbals0@naver.com>
2022-08-22 10:35:13 +0900

    pm2 config file

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [{
+    name: "elc_application",
+    script: 'npm',
+    args: "start",
+  }],
+
+  // deploy : {
+  //   production : {
+  //     user : 'SSH_USERNAME',
+  //     host : 'SSH_HOSTMACHINE',
+  //     ref  : 'origin/master',
+  //     repo : 'GIT_REPOSITORY',
+  //     path : 'DESTINATION_PATH',
+  //     'pre-deploy-local': '',
+  //     'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+  //     'pre-setup': ''
+  //   }
+  // }
+};
