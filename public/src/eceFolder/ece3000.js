b38f19ee56fcfd9a675d3139df0fbe1f6fb5fd7b
Yumin-Kim <dbals0@naver.com>
2022-09-26 14:24:24 +0900

    ece3700

--- public/src/eceFolder/ece3000.js
+++ public/src/eceFolder/ece3000.js
@@ -128,66 +128,70 @@ AJAXRequestMethod({ method: "GET", requestURL: `${serverURL}/ece1000` })
         const { data: validMinig } = await AJAXRequestMethod({ method: "GET", requestURL: `${serverURL}/ece3000/ece3401_beta?member=${member}` })
         if (validMinig.length !== 0) {
             $('#mining').fadeIn();
-            $("#mining").click(async () => {
-                const response = await AJAXRequestMethod({ method: "GET", requestURL: `${serverURL}/ece3000/ece3400_beta?member=${member}` })
-                if (response.status === 1320) {
-                    $("#snackbar").html('<h3>체굴 실패</h3><p>채굴할 자원이 없습니다.<p/>')
-                } else {
-                    let sp = ""
-                    const text = response.data.map((v) => {
-                        if (v.category !== 7601) {
-                            const description = selectCodeNameTpCodeTable({ data, codeName: v.category })
-                            return `${description}: ${v.amount}`
-                        } else {
-                            const description = selectCodeNameTpCodeTable({ data, codeName: v.category })
-                            sp = `${description}: ${v.amount}`
-                        }
-                    })
-                    $("#snackbar").html(`
-                <h3>채굴 완료</h3>
-                <p>${text.join(",").replaceAll(",", " ")}</p>
-                <p>${sp}</p>`);
-                    clearInterval(trickTimer)
-                    const { data: resourceBannerText } = await AJAXRequestMethod({ method: "GET", requestURL: `${serverURL}/ece3000/ece3500_beta?member=${member}` })
-                    let bannerList = "채굴된 자원이 없습니다."
-                    if (resourceBannerText.resoureList.length !== 0) {
-                        bannerList = resourceBannerText.resoureList.map((v) => {
-                            const description = selectCodeNameTpCodeTable({ data, codeName: v.resource })
-                            if (description === "동") {
-                                return `${description}: <span id ="resourceTrick">${v.amount}</span>`
-                            } else {
+            if (location.pathname === "/ece3601.html") {
+                $("#mining").click(async () => {
+                    const [miner, minerId] = location.search.split("&")[1].split("=");
+                    const response = await AJAXRequestMethod({ method: "GET", requestURL: `${serverURL}/ece3000/ece3400_beta_01?member=${member}&miner=${minerId}` })
+                    if (response.status === 1320) {
+                        $("#snackbar").html('<h3>체굴 실패</h3><p>채굴할 자원이 없습니다.<p/>')
+                    } else {
+                        let sp = ""
+                        const text = response.data.map((v) => {
+                            if (v.category !== 7601) {
+                                const description = selectCodeNameTpCodeTable({ data, codeName: v.category })
                                 return `${description}: ${v.amount}`
+                            } else {
+                                const description = selectCodeNameTpCodeTable({ data, codeName: v.category })
+                                sp = `${description}: ${v.amount}`
                             }
                         })
-                        bannerList = bannerList.join(",").replaceAll(",", " ")
+                        $("#snackbar").html(`
+                    <h3>채굴 완료</h3>
+                    <p>${text.join(",").replaceAll(",", " ")}</p>
+                    <p>${sp}</p>`);
+                        clearInterval(trickTimer)
+                        const { data: resourceBannerText } = await AJAXRequestMethod({ method: "GET", requestURL: `${serverURL}/ece3000/ece3500_beta?member=${member}` })
+                        let bannerList = "채굴된 자원이 없습니다."
+                        if (resourceBannerText.resoureList.length !== 0) {
+                            bannerList = resourceBannerText.resoureList.map((v) => {
+                                const description = selectCodeNameTpCodeTable({ data, codeName: v.resource })
+                                if (description === "동") {
+                                    return `${description}: <span id ="resourceTrick">${v.amount}</span>`
+                                } else {
+                                    return `${description}: ${v.amount}`
+                                }
+                            })
+                            bannerList = bannerList.join(",").replaceAll(",", " ")
+                        }
+                        $('#resourceText').html(bannerList)
+                        const trickResource = $("#resourceTrick").text()
+                        trickTimer = setInterval(() => {
+                            const unixtime = Math.floor(new Date().getTime() / 1000)
+                            $("#resourceTrick").text(`${trickResource}.${String(unixtime).slice(5, 10)}`)
+                        }, 1000)
                     }
-                    $('#resourceText').html(bannerList)
-                    const trickResource = $("#resourceTrick").text()
-                    trickTimer = setInterval(() => {
-                        const unixtime = Math.floor(new Date().getTime() / 1000)
-                        $("#resourceTrick").text(`${trickResource}.${String(unixtime).slice(5, 10)}`)
-                    }, 1000)
-                }
-                $("#mining").css("display", "none")
-                let check = false;
-                // const clearTimer = window.setInterval(() => {
-                //     let today = new Date();
-                //     let minutes = today.getMinutes();  // 분
-                //     let seconds = today.getSeconds();  // 초
-                //     if (!check) {
-                //         $("#timer").text(`채굴 완료 까지 ${60 - Number(minutes)}:${60 - Number(seconds)} 남았습니다.`);
-                //         if (seconds === 59 && minutes === 59) {
-                //             check = true;
-                //         }
-                //     }
-                //     else {
-                //         $("#timer").text("채굴하기를 눌러주세요")
-                //         $("#mining").css("display", "block")
-                //         clearInterval(clearTimer)
+                    $("#mining").css("display", "none")
+                    let check = false;
+                    // const clearTimer = window.setInterval(() => {
+                    //     let today = new Date();
+                    //     let minutes = today.getMinutes();  // 분
+                    //     let seconds = today.getSeconds();  // 초
+                    //     if (!check) {
+                    //         $("#timer").text(`채굴 완료 까지 ${60 - Number(minutes)}:${60 - Number(seconds)} 남았습니다.`);
+                    //         if (seconds === 59 && minutes === 59) {
+                    //             check = true;
+                    //         }
+                    //     }
+                    //     else {
+                    //         $("#timer").text("채굴하기를 눌러주세요")
+                    //         $("#mining").css("display", "block")
+                    //         clearInterval(clearTimer)
 
-                //     }
-                // }, 1000)
-            })
+                    //     }
+                    // }, 1000)
+                })
+
+            }
         }
 
     })
